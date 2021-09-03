1b39c475b5f04deb86d817a906f1bdfbcc691e3c
Elena Shustova <alenyf-ka@mail.ru>
2021-09-03 13:26:57 +0300

    complete task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    minValue;
+    maxValue;
+    number;
+    guessNumber;
 
+    setRange(min, max) {
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.round((this.minValue + this.maxValue) / 2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.maxValue = this.guessNumber;
     }
 
     greater() {
-
+        this.minValue = this.guessNumber;
     }
 }
 
